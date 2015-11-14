921be684c69dd99db28c1e59af44950b17918b86
novirael <novirael@gmail.com>
2015-11-14 03:02:42 +0100

    Add setup and manifest files

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-projects-manager',
+    version='0.1',
+    packages=['projects_manager'],
+    include_package_data=True,
+    license='GNU v2 License',
+    description='A simple Django app to manage projects with Trello.',
+    long_description=README,
+    author='Rafal Nowicki',
+    author_email='novirael@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
